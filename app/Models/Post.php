2762ba829f6a027d6071354f1ddb8704ff06f66f
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2023-11-23 12:10:31 +0800

    post detail

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    public $timestamps = true;
+
+    protected $table = 'posts';
+
+    protected $primaryKey = 'id';
+
+    public $incrementing = true;
+
+    protected $guarded = ['id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class, 'post_id', 'id');
+    }
+
 }
